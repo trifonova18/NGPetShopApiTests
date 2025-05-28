d8dbacbfae7546a953879f5ae51e2fdcadd70f4e
fakeg <natalia.o.g@mail.ru>
2025-05-28 22:16:34 +0300

    Добавила тест "Попытка обновить несуществующего животного"

--- test/test_pet.py
+++ test/test_pet.py
@@ -17,3 +17,18 @@ class TestPet:
 
         with allure.step("Проверка тестового содержимого ответа"):
             assert response.text == "Pet deleted", "Текст ответа не совпал с ожидаемым"
+
+    @allure.title("Попытка обновить несуществующего животного")
+    def test_update_noneexistent_pet(self):
+        with allure.step("Отправка запроса на обновление несущесвующего питомца"):
+            payload = {"id": 9999,
+                       "name": "Non-existent Pet",
+                       "status": "available"
+                       }
+            response = requests.put(f"{BASE_URL}/pet", json=payload)
+
+        with allure.step("Проверка статуса ответа"):
+            assert  response.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка тестового содержимого ответа"):
+            assert response.text == "Pet not found", "Текст ответа не совпал с ожидаемым"
